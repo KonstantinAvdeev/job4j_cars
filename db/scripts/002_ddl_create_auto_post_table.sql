7da363d4b62986bf2b71cd857ea6c829a9419ee4
Konstantin Avdeev <fujiun91@gmail.com>
2023-03-30 20:10:07 +0300

    2. Доменная модель проекта "АвтоМаг" [#504897]

---
+++ db/scripts/002_ddl_create_auto_post_table.sql
@@ -0,0 +1,7 @@
+create table auto_post
+(
+    id           serial primary key,
+    description  varchar,
+    created      timestamp not null,
+    auto_user_id int       not null references auto_user (id)
+);
